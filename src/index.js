20cf5a85cd895cf4e57d146051e808583708129d
Eugene <exsoulck@gmail.com>
2023-03-10 21:48:29 -0500

    Updates

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import {initializeApp} from "firebase/app"
 import {
-    getFirestore, collection, getDocs, addDoc
+    getFirestore, collection, getDocs, addDoc, onSnapshot, serverTimestamp, query, orderBy, limit
 } from "firebase/firestore"
+
+
 const firebaseConfig = {
 
     apiKey: "AIzaSyDktEjxL2gKjh7KroVKkNE3uFebWhsBqQU",
@@ -21,19 +23,72 @@ const firebaseConfig = {
 };
 
 initializeApp(firebaseConfig);
+
 const db = getFirestore()
 const colRef = collection(db, "chat")
-addDoc(colRef, {
-    message: "test msg",
-    name:" test name"
+const userInputMsg = document.querySelector("#usr")
+const userName = document.querySelector(".new-name")
+const chatList = document.querySelector(".messages-chat")
+
+let localNewUser = []
+userName.addEventListener("submit", evt => {
+    evt.preventDefault()
+    let newUser = userName.querySelector("#name").value
+    localNewUser = newUser
+    console.log(localNewUser)
+    userName.reset()
+
+})
+function loadMessages() {
+  // Create the query to load the last 12 messages and listen for new ones.
+  const recentMessagesQuery = query(collection(getFirestore(), 'chat'), orderBy('timestamp', 'desc'), limit(12));
+    onSnapshot(recentMessagesQuery, function(snapshot) {
+    snapshot.docChanges().forEach(function(change) {
+      if (change.type === 'added') {
+        let injectHtml = `
+              <li class="message text-only">
+                 <span class="text"><strong>${change.doc.data()["name"]}:  </strong></span>
+                <span class="text">${change.doc.data()["message"]}</span></li>
+                <p class="time">${change.doc.data()["timestamp"]}</p>
+              `
+            chatList.innerHTML += injectHtml;
+      }})})}
+
+loadMessages()
+// getDocs(colRef).then(snapshot => {
+//     snapshot.docChanges().forEach(change => {
+//         if (change.type === "added") {
+//             let injectHtml = `
+//               <li class="message text-only">
+//                  <span class="text"><strong>${change.doc.data()["name"]}:  </strong></span>
+//                 <span class="text">${change.doc.data()["message"]}</span></li>
+//                 <p class="time">${change.doc.data()["timestamp"]}</p>
+//               `
+//             chatList.innerHTML += injectHtml
+//         }
+//     })
+// })
+
+
+userInputMsg.addEventListener("submit", evt => {
+    evt.preventDefault();
+    let msg = userInputMsg.querySelector("input")
+    let name = ""
+    if (localNewUser.length === 0) {
+        name = "anon";
+    } else {
+        name = localNewUser
+    }
+    addDoc(colRef, {
+        message: msg.value,
+        name: name,
+        timestamp: serverTimestamp()
+
+    })
+    userInputMsg.reset()
+
 })
 
-getDocs(colRef)
-    .then((snap) =>{
-        let lisT = []
-        snap.docs.forEach((doc) =>{
-            lisT.push({...doc.data()})
-        })
-        console.log(lisT)
-    })
+
+
 
