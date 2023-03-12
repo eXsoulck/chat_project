20bb5e8a3253894c61224078da9374627125a176
Eugene <exsoulck@gmail.com>
2023-03-11 21:20:03 -0500

    Updates

--- src/index.js
+++ src/index.js
@@ -5,31 +5,21 @@ import {
 
 
 const firebaseConfig = {
-
     apiKey: "AIzaSyDktEjxL2gKjh7KroVKkNE3uFebWhsBqQU",
-
     authDomain: "chatroom-293fd.firebaseapp.com",
-
     projectId: "chatroom-293fd",
-
     storageBucket: "chatroom-293fd.appspot.com",
-
     messagingSenderId: "982794611788",
-
     appId: "1:982794611788:web:edd642b23840c02db018e9",
-
     measurementId: "G-N7LEMQTE6C"
-
 };
 
 initializeApp(firebaseConfig);
-
 const db = getFirestore()
 const colRef = collection(db, "chat")
 const userInputMsg = document.querySelector("#usr")
 const userName = document.querySelector(".new-name")
 const chatList = document.querySelector(".messages-chat")
-
 
 let localNewUser = []
 userName.addEventListener("submit", evt => {
@@ -38,19 +28,14 @@ userName.addEventListener("submit", evt => {
     localNewUser = newUser
     console.log(localNewUser)
     userName.reset()
-
 })
-const now = new Date()
 
+const now = new Date()
 const currentRoom = document.querySelector(".chat-rooms");
 let room = "#general"
 
-
-
-
-
 function loadMessages(room) {
-    // Create the query to load the last 12 messages and listen for new ones.
+    // Create the query to load the last 10 messages and listen for new ones.
     const recentMessagesQuery = query(collection(getFirestore(), 'chat'), where("room", "==", room), limit(10));
     onSnapshot(recentMessagesQuery, function (snapshot) {
         snapshot.docChanges().forEach(function (change) {
@@ -59,36 +44,20 @@ function loadMessages(room) {
               <li class="message text-only">
                  <span class="text"><strong>${change.doc.data()["name"]}:  </strong></span>
                 <span class="text">${change.doc.data()["message"]}</span></li>
-                <p class="time">${now.toTimeString()}</p>
-              `
+                <p class="time">${now.toTimeString()}</p> `
                 chatList.innerHTML += injectHtml;
 
             }
         })
     })
 }
+
 currentRoom.addEventListener("click", evt => {
     room = evt.target.textContent
     chatList.innerHTML = ""
     loadMessages(room)
 
 })
-// loadMessages(room)
-
-// getDocs(colRef).then(snapshot => {
-//     snapshot.docChanges().forEach(change => {
-//         if (change.type === "added") {
-//             let injectHtml = `
-//               <li class="message text-only">
-//                  <span class="text"><strong>${change.doc.data()["name"]}:  </strong></span>
-//                 <span class="text">${change.doc.data()["message"]}</span></li>
-//                 <p class="time">${change.doc.data()["timestamp"]}</p>
-//               `
-//             chatList.innerHTML += injectHtml
-//         }
-//     })
-// })
-
 userInputMsg.addEventListener("submit", evt => {
     evt.preventDefault();
     let msg = userInputMsg.querySelector("input")
